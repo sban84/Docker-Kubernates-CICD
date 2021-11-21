a18f8029b8cf90cde330942eaf68c890a25d5b94
Suman Banerjee <java2suman@gmail.com>
2021-11-21 12:32:02 +0100

    feat(calculator): add intial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import math
+
+
+class Calculator:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def add(self, x, y):
+        return self.x + self.y
+
+    def subtract(self, x, y):
+        pass
+
+
+if __name__ == "__main__":
+    a, b = 2, 3
+    cal = Calculator(2, 3)
+    print(f"addition of {a} and {b} = {cal.add(a, b)}")
+    print(f"subtraction of {a} and {b} = {cal.subtract(a, b)}")
